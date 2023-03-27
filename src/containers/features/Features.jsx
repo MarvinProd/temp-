499db902018d753d27ca50014f788a1cc9d75fe1
Marvin <marvinmaduike@gmail.com>
2023-03-27 06:28:05 +0100

    new

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Feature } from '../../components'
+import './features.css'
+
+const featuresData = 
+[
+  {
+    id: 1,
+    title: 'Improving end distrusts instantly ',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+   
+  },
+
+  {
+    id: 2,
+    title: 'Become the tended active ',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+   
+  },
+
+  {
+    id: 3,
+    title: 'Message or am nothing',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+   
+  },
+
+  {
+    id: 4,
+    title: 'Really boy law county',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
+   
+  },
+
+]
+
+function Features() {
+  return (
+    <div className='gpt3__features section__padding'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>
+
+        The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+
+      </div>
+
+      <div className='gpt3__features-container'>
+
+        {featuresData.map((item,index) => (
+          <Feature 
+          title={item.title} text={item.text} key={item.id}></Feature>
+        ))}
+
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Features
---
+++ src/containers/whatGTP3/WhatGTP3.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Feature } from '../../components'
+import './whatGTP3.css'
+
+function WhatGTP3() {
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gtp3__whatgpt3-feature'>
+        <Feature title= 'What is GTP-3 ' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+          The possibilites are beyond your imagination
+        </h1>
+        <p>
+          Explore The Library
+        </p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title='Chatbots' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. '/>
+        <Feature title='Knowledgebase' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. '/>
+        <Feature title='Education' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. '/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatGTP3
